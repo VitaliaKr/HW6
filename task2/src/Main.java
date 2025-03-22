a0f8264a606c3f59f3014b099de97abadce01a93
VitaliaKr <81364950+VitaliaKr@users.noreply.github.com>
2025-03-22 22:29:36 +0400

    done

--- task2/src/Main.java
+++ task2/src/Main.java
@@ -17,18 +17,32 @@ public class Main {
         double average = 0;
         double smallest = 0;
         //todo дописать логику программы ниже.
-
-
-
-
-
-
+        if (first > second) {
+            greatest = first;
+            smallest = second;
+        } else {
+            greatest = second;
+            smallest = first;
+        }
+        if (third > greatest) {
+            greatest = third;
+        }
+        if (third < smallest) {
+            smallest = third;
+        }
+        if (first != greatest && first != smallest) {
+            average = first;
+        } else if (second != greatest && second != smallest) {
+            average = second;
+        } else if (third != greatest && third != smallest) {
+            average = third;
+        }
 
         //todo
 
 
-        System.out.println("Наибольший вес: " + 10);
-        System.out.println("Средний вес: " + 9);
-        System.out.println("Наименьший вес: " + 1);
+        System.out.println("Наибольший вес: " + greatest);
+        System.out.println("Средний вес: " + average);
+        System.out.println("Наименьший вес: " + smallest);
     }
 }
